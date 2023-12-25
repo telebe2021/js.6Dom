b870224d783ef562ae00672363052c9e37be82dc
telebe2021 <mahsungulmmmdov@gmail.com>
2023-12-25 12:36:17 +0400

    commit olundu

--- script.js
+++ script.js
@@ -3,60 +3,62 @@ let borderRadius = 0;
 let heightValue = 60;
 let widthValue = 180;
 
+const textDiv = document.getElementById("textDiv");
+
 function artirFont() {
   fontSize += 5;
-  textDiv.index.fontSize = fontSize + "px";
+  textDiv.style.fontSize = fontSize + "px";
 }
 
 function azaltFont() {
   if (fontSize > 5) {
-    fontSize = fontSize - 5;
-    textDiv.index.fontSize = fontSize + "px";
+    fontSize -= 5;
+    textDiv.style.fontSize = fontSize + "px";
   }
 }
 
 function artirRadius() {
   borderRadius += 5;
-  textDiv.index.borderRadius = borderRadius + "px";
+  textDiv.style.borderRadius = borderRadius + "px";
 }
 
 function azaltRadius() {
   if (borderRadius > 0) {
     borderRadius -= 5;
-    textDiv.index.borderRadius = borderRadius + "px";
+    textDiv.style.borderRadius = borderRadius + "px";
   }
 }
 
 function artirHeight() {
   heightValue += 5;
-  textDiv.index.height = heightValue + "px";
+  textDiv.style.height = heightValue + "px";
 }
 
 function azaltHeight() {
   if (heightValue > 5) {
     heightValue -= 5;
-    textDiv.index.height = heightValue + "px";
+    textDiv.style.height = heightValue + "px";
   }
 }
 
 function artirWidth() {
   widthValue += 5;
-  textDiv.index.width = widthValue + "px";
+  textDiv.style.width = widthValue + "px";
 }
 
 function azaltWidth() {
   if (widthValue > 5) {
     widthValue -= 5;
-    textDiv.index.width = widthValue + "px";
+    textDiv.style.width = widthValue + "px";
   }
 }
 
-function deyishTextColor() {
+function deyisTextColor() {
   const textColorInput = document.getElementById("textColorInput");
-  textDiv.index.color = textColorInput.value;
+  textDiv.style.color = textColorInput.value;
 }
 
-function deyishBackgroundColor() {
+function deyisBackgroundColor() {
   const bgColorInput = document.getElementById("bgColorInput");
-  textDiv.index.backgroundColor = bgColorInput.value;
+  textDiv.style.backgroundColor = bgColorInput.value;
 }
